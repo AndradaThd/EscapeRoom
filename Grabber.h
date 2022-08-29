d64a6b652396dca9c089b382a68c950b7cc740c7
AndradaThd <75580814+AndradaThd@users.noreply.github.com>
2022-08-29 03:40:56 +0300

    Update Grabber.h

--- Grabber.h
+++ Grabber.h
@@ -1,4 +1,3 @@
-// Fill out your copyright notice in the Description page of Project Settings.
 
 #pragma once
 
@@ -14,15 +13,12 @@ class CRYPTRAIDER_API UGrabber : public USceneComponent
 	GENERATED_BODY()
 
 public:	
-	// Sets default values for this component's properties
 	UGrabber();
 
 protected:
-	// Called when the game starts
 	virtual void BeginPlay() override;
 
 public:	
-	// Called every frame
 	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
 
 	UFUNCTION(BlueprintCallable)
